dd79774147cee0f6a0e98262c23cb1b83be9474c
Ibrahima Traore <whodunit@Ibrahimas-MacBook-Air.local>
2020-08-11 16:58:44 +0200

    new content repo

--- post-update/etc/run.d/10-bower-install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-#bower --config.directory=/srv/www/js/bower-components/ --allow-root install /srv/www/bower.json
--- post-update/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for f in `ls /etc/run.d/`
-do
-	sh /etc/run.d/$f
-done
-
-/usr/bin/supervisord
